bee2145a687ee7c5aaa0581213fd0a42cbb2c5f3
arafeh198@gmail.com <pmdudmp123>
2019-07-15 14:58:58 +0300

    fix issues

--- network-local-mcs-mini.sh
+++ network-local-mcs-mini.sh
@@ -4,37 +4,67 @@ function info() {
     echo -e "************************************************************\n\033[1;33m${1}\033[m\n************************************************************"
 }
 
-export DOMAIN=mcs.com
+export DOMAIN=${DOMAIN-example.com}
+
+orgs=${@:-org1}
+first_org=${1:-org1}
+
+channel=${CHANNEL:-common}
+chaincode_install_args=${CHAINCODE_INSTALL_ARGS:-reference}
+chaincode_instantiate_args=${CHAINCODE_INSTANTIATE_ARGS:-common reference}
+docker_compose_args=${DOCKER_COMPOSE_ARGS:- -f docker-compose.yaml -f docker-compose-couchdb.yaml -f docker-compose-dev.yaml}
+
+# Clean up. Remove all containers, delete local crypto material
 
 info "Cleaning up"
 ./clean.sh
+unset ORG COMPOSE_PROJECT_NAME
 
+# Create orderer organization
 
-info "Creating Orderer"
+info "Creating orderer organization for $DOMAIN"
 docker-compose -f docker-compose-orderer.yaml -f docker-compose-orderer-ports.yaml up -d
 
+# Create member organizations
 
-info "Creating member organization Server with api 4000"
-export ORG=server API_PORT=4000 WWW_PORT=81 PEER0_PORT=7054 CA_PORT=7051
+api_port=${API_PORT:-4000}
+#dev:
+www_port=${WWW_PORT:-81}
+ca_port=${CA_PORT:-7054}
+peer0_port=${PEER0_PORT:-7051}
+#
+
+export ORG=server API_PORT=${api_port} WWW_PORT=${www_port} PEER0_PORT=${peer0_port} CA_PORT=${ca_port}
 export COMPOSE_PROJECT_NAME=server
-docker-compose -f docker-compose.yaml -f docker-compose-couchdb.yaml -f docker-compose-dev.yaml up -d
+info "Creating member organization $ORG with api $API_PORT"
+docker-compose ${docker_compose_args} up -d
+api_port=$((api_port + 1))
+www_port=$((www_port + 1))
+ca_port=$((ca_port + 1))
+peer0_port=$((peer0_port + 1000))
 unset ORG COMPOSE_PROJECT_NAME API_PORT WWW_PORT PEER0_PORT CA_PORT
 
 
-info "Adding Server to the consortium"
+# Add member organizations to the consortium
+
+
+info "Adding $org to the consortium"
 ./consortium-add-org.sh server
 
 
+# First organization creates the channel
 
-info "Setting Up Server Organization"
 export ORG=server
 export COMPOSE_PROJECT_NAME=server
 
-info "Server - channel: users"
-./channel-create.sh users
-./channel-join.sh users
+info "Creating channel ${channel} by $ORG"
+./channel-create.sh common
+./channel-join.sh common
 
-info "Server - chaincode install"
-./chaincode-install.sh ccusers
-./chaincode-instantiate.sh users ccusers '["init","a","10","b","0"]'
+
+# First organization creates the chaincode
+
+info "Creating chaincode by $ORG: ${chaincode_install_args} ${chaincode_instantiate_args}"
+./chaincode-install.sh relationship
+./chaincode-instantiate.sh common relationship '["init","a","10","b","0"]'
 
--- network-local-mcs.sh
+++ network-local-mcs.sh
@@ -4,66 +4,76 @@ function info() {
     echo -e "************************************************************\n\033[1;33m${1}\033[m\n************************************************************"
 }
 
-export DOMAIN=mcs.com
+export DOMAIN=${DOMAIN-msg.com}
+
+orgs=${@:-org1}
+first_org=${1:-org1}
+
+docker_compose_args=${DOCKER_COMPOSE_ARGS:- -f docker-compose.yaml -f docker-compose-couchdb.yaml -f docker-compose-dev.yaml}
+
+# Clean up. Remove all containers, delete local crypto material
 
 info "Cleaning up"
 ./clean.sh
+unset ORG COMPOSE_PROJECT_NAME
 
+# Create orderer organization
 
-info "Creating Orderer"
+info "Creating orderer organization for $DOMAIN"
 docker-compose -f docker-compose-orderer.yaml -f docker-compose-orderer-ports.yaml up -d
 
 
-info "Creating member organization Server with api 4000"
-export ORG=server API_PORT=4000 WWW_PORT=81 PEER0_PORT=7054 CA_PORT=7051
+api_port=${API_PORT:-4000}
+www_port=${WWW_PORT:-81}
+ca_port=${CA_PORT:-7054}
+peer0_port=${PEER0_PORT:-7051}
+
+export ORG=server API_PORT=${api_port} WWW_PORT=${www_port} PEER0_PORT=${peer0_port} CA_PORT=${ca_port}
 export COMPOSE_PROJECT_NAME=server
-docker-compose -f docker-compose.yaml -f docker-compose-couchdb.yaml -f docker-compose-dev.yaml up -d
+info "Creating member organization server with api $API_PORT"
+docker-compose ${docker_compose_args} up -d
+api_port=$((api_port + 1))
+www_port=$((www_port + 1))
+ca_port=$((ca_port + 1))
+peer0_port=$((peer0_port + 1000))
 unset ORG COMPOSE_PROJECT_NAME API_PORT WWW_PORT PEER0_PORT CA_PORT
 
-info "Creating member organization Worker with api 4001"
-export ORG=worker API_PORT=4001 WWW_PORT=82 PEER0_PORT=8054 CA_PORT=7052
+export ORG=worker API_PORT=${api_port} WWW_PORT=${www_port} PEER0_PORT=${peer0_port} CA_PORT=${ca_port}
 export COMPOSE_PROJECT_NAME=worker
-docker-compose -f docker-compose.yaml -f docker-compose-couchdb.yaml -f docker-compose-dev.yaml up -d
+info "Creating member organization worker with api $API_PORT"
+docker-compose ${docker_compose_args} up -d
+api_port=$((api_port + 1))
+www_port=$((www_port + 1))
+ca_port=$((ca_port + 1))
+peer0_port=$((peer0_port + 1000))
 unset ORG COMPOSE_PROJECT_NAME API_PORT WWW_PORT PEER0_PORT CA_PORT
 
-info "Adding Server to the consortium"
-./consortium-add-org.sh server
 
-info "Adding Worker to the consortium"
+info "Adding server to the consortium"
+./consortium-add-org.sh server
+info "Adding worker to the consortium"
 ./consortium-add-org.sh worker
 
 
-info "Setting Up Server Organization"
 export ORG=server
 export COMPOSE_PROJECT_NAME=server
 
-info "Server - channel: users"
+info "Server - Creating Channels"
 ./channel-create.sh users
-./channel-join.sh users
-
-
-info "Server - channel: tasks"
 ./channel-create.sh tasks
-./channel-add-org.sh tasks worker
-./channel-join.sh tasks
-
-info "Server - channel: TaskParticipants"
 ./channel-create.sh taskpts
-./channel-add-org.sh taskpts worker
-./channel-join.sh taskpts
-
-
-info "Server - channel: reputations"
 ./channel-create.sh reputations
-./channel-add-org.sh reputations worker
-./channel-join.sh reputations
-
-
-info "Server - channel: reports"
 ./channel-create.sh reports
+info "Server - Join Channels"
+./channel-join.sh users
+./channel-join.sh tasks
+./channel-join.sh taskpts
+./channel-join.sh reputations
 ./channel-join.sh reports
-
-info "Server - chaincode install"
+./channel-add-org.sh tasks worker
+./channel-add-org.sh taskpts worker
+./channel-add-org.sh reputations worker
+info "Server - Install chaincode"
 ./chaincode-install.sh ccusers
 ./chaincode-install.sh cctasks
 ./chaincode-install.sh cctaskpts
@@ -75,26 +85,14 @@ info "Server - chaincode install"
 ./chaincode-instantiate.sh reputations ccreputations '["init","a","10","b","0"]'
 ./chaincode-instantiate.sh reports ccreports '["init","a","10","b","0"]'
 
-
-info "Setting Up Worker Organization"
-export ORG=worker
-export COMPOSE_PROJECT_NAME=worker
-
-info "Worker - channel: tasks"
-./channel-join.sh tasks
-
-info "Worker - channel: TaskParticipants"
-./channel-join.sh taskpts
-
-info "Worker - channel: reputations"
-./channel-join.sh reputations
-
-info "Worker - channel: observations"
+info "Worker - Creating Channels"
 ./channel-create.sh observations
+info "Worker - Join Channels"
 ./channel-join.sh observations
-
-
-info "Worker - chaincode install"
+./channel-join.sh tasks
+./channel-join.sh taskpts
+./channel-join.sh reputations
+info "Worker - Install chaincode"
 ./chaincode-install.sh cctasks
 ./chaincode-install.sh cctaskpts
 ./chaincode-install.sh ccreputations
